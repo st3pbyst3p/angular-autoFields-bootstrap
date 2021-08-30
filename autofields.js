d3dab44fb1f0b94de135bf6c0f9a9b6b1524776e
st3pbyst3p <cornel.scoarta@gmail.com>
2021-08-30 15:24:12 +0300

    Added custom directive to standart input

--- autofields.js
+++ autofields.js
@@ -281,7 +281,8 @@ angular.module('autofields.standard',['autofields.core'])
 
 		$autofieldsProvider.settings.fixUrl = true;
 		$autofieldsProvider.registerHandler(['text','email','url','date','number','password'], function(directive, field, index){
-			var htmlTemplate = '<input/>';
+			// var htmlTemplate = '<input/>';
+			var htmlTemplate = '<input alt-clear-ngmodel />';
 			var fieldElements = $autofieldsProvider.field(directive, field, htmlTemplate);
 			var t = fieldElements.input[0].placeholder;
 
